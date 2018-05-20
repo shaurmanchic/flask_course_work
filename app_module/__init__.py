b4225b14bc0443a083e0a6f097299176b60367d7
Vitalii <vitalii.liashchenko@gmail.com>
2018-05-20 09:57:19 +0300

    Added background workers

--- app_module/__init__.py
+++ app_module/__init__.py
@@ -12,6 +12,8 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from elasticsearch import Elasticsearch
+from redis import Redis
+import rq
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -35,6 +37,9 @@ def create_app(config_class=Config):
     bootstrap.init_app(app)
     moment.init_app(app)
     babel.init_app(app)
+
+    app.redis = Redis.from_url(app.config['REDIS_URL'])
+    app.task_queue = rq.Queue('microblog-tasks', connection=app.redis)
 
     from app_module.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
